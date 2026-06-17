adcf9ee0e827999e571c2ca986490b0f480729ef
jema <smh1141@naver.com>
2026-06-17 09:19:14 +0900

    feat : header.js 코드 수정 및 링크 연결

--- js/module/header.js
+++ js/module/header.js
@@ -5,7 +5,7 @@ export function renderHeader() {
     <div class="header-wrapper">
         <div class="header-left">
           <h1 class="logo">
-            <a href="/">
+            <a href="index.html">
               <img src="./img/LOGO-img.png" alt="ROUNZ" />
               <span class="blind">ROUNZ 로고</span>
             </a>
@@ -15,24 +15,24 @@ export function renderHeader() {
               <li><a href="#">안경</a></li>
               <li><a href="#">선글라스</a></li>
               <li><a href="#">AI TRY-ON</a></li>
-              <li><a href="#">고객센터</a></li>
+              <li><a href="support.html">고객센터</a></li>
             </ul>
           </nav>
         </div>
 
         <div class="header-actions">
-          <button class="icon-btn search-btn">
+          <a href="#">
             <span class="material-symbols-outlined">search</span>
             <span class="blind">검색</span>
-          </button>
-          <button class="icon-btn user-btn">
+          </a>
+          <a href="login.html">
             <span class="material-symbols-outlined">person</span>
             <span class="blind">마이페이지</span>
-          </button>
-          <button class="icon-btn cart-btn">
+          </a>
+          <a href="#">
             <span class="material-symbols-outlined">local_mall</span>
             <span class="blind">장바구니</span>
-          </button>
+          </a>
         </div>
       </div>
   `;
